8d295c5ad394e6d5746e68851985589a6beaddc7
misostack <misostack.com@gmail.com>
2023-06-20 17:59:31 +0700

    add public assets

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    // public folder
+    ServeStaticModule.forRoot({
+      rootPath: join(process.cwd(), 'public'),
+      serveRoot: '/public',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
